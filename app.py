ded0a6ce66d120b95394862448ffac7ae1261939
Fin1041 <Fin1041@naver.com>
2026-09-02 22:04:11 +0900

    Update top title in markdown for AI chatbot

--- app.py
+++ app.py
@@ -389,7 +389,7 @@ if "rulebook_category" not in st.session_state:
 
 
 st.markdown(
-    '<div class="top-title">🏠 대구경북지사 기술업무 AI 챗봇 🤖</div>',
+    '<div class="top-title">🏠 대구경북지사 AI 챗봇 </div>',
     unsafe_allow_html=True
 )
 
